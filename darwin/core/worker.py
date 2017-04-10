c5c8b36be7c93ad5daecac3457fbaa566dbe4722
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-04-10 10:36:43 +0200

    - finished worker's socket business

--- darwin/core/worker.py
+++ darwin/core/worker.py
@@ -1,33 +1,63 @@
+import time
 import zmq
 import Antipasti.legacy.netdatautils as ndu
 
 
 class WorkerBase(object):
-    def __init__(self, worker_id=None, pull_from_port=None, push_to_port=None):
+    def __init__(self, worker_id=None, pull_from_address=None, push_to_address=None):
         """
         Abstract class for worker to handle network comms. No computation is to be done here.
 
         :param worker_id:
-        :param pull_from_port:
-        :param push_to_port:
+        :param pull_from_address:
+        :param push_to_address:
         """
+        # Private
+        self._zmq_context = zmq.Context()
+        self._push_socket = None
+        self._pull_socket = None
+        self._pulled = None
+        self._pushed = None
+        self._last_pulled = None
+        self._last_pushed = None
+        # Public
         self.worker_id = worker_id
-        self.pull_port = pull_from_port
-        self.push_port = push_to_port
+        self.pull_address = pull_from_address
+        self.push_address = push_to_address
         # TODO
         pass
 
-    def connect_to_ports(self):
-        # TODO
-        pass
+    @property
+    def push_socket(self):
+        if self._push_socket is None:
+            self._push_socket = self._zmq_context.socket(zmq.PUSH)
+            self._push_socket.connect(self.push_address)
+        return self._push_socket
+
+    @property
+    def pull_socket(self):
+        if self._pull_socket is None:
+            self._pull_socket = self._zmq_context.socket(zmq.PULL)
+            self._pull_socket.connect(self.pull_address)
+        return self._pull_socket
+
+    def get_from_pulled(self, key, default=None):
+        return self._pulled.get(key, default)
+
+    def add_to_push(self, key, value):
+        return self._pushed.update({key: value})
 
     def pull(self):
-        # TODO
-        pass
+        """Pull from server. Cache is destroyed."""
+        self._pulled = self.pull_socket.recv_json()
+        self._last_pulled = time.time()
 
     def push(self):
-        # TODO
-        pass
+        """Push to server. Cache is destroyed."""
+        self.push_socket.send_json(self._pushed)
+        # Flush self._pushed
+        self._pushed = {}
+        self._last_pushed = time.time()
 
     def work(self):
         raise NotImplementedError
@@ -36,6 +66,9 @@ class WorkerBase(object):
         config = ndu.yaml2dict(config_file)
 
         self.worker_id = config.get('worker_id', self.worker_id)
-        self.pull_port = config.get('pull_from_port', self.pull_port)
-        self.push_port = config.get('push_to_port', self.push_port)
+        self.pull_address = config.get('pull_from_address', self.pull_address)
+        self.push_address = config.get('push_to_address', self.push_address)
 
+
+class Worker(WorkerBase):
+    pass
