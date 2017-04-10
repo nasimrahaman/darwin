b19ebc48657cc24593df9089a99704a4001b58fd
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-04-10 14:34:31 +0200

    - finished listener

--- darwin/core/worker.py
+++ darwin/core/worker.py
@@ -1,6 +1,8 @@
 import time
 import zmq
+import threading
 import Antipasti.legacy.netdatautils as ndu
+import Antipasti.utilities.pyutils2 as py2
 
 
 class WorkerBase(object):
@@ -20,6 +22,12 @@ class WorkerBase(object):
         self._pushed = None
         self._last_pulled = None
         self._last_pushed = None
+        self._listener_latency = 1
+        self._successfully_pulled_from_server = threading.Event()
+        self._stop_listening_event = threading.Event()
+        self._listener_thread = None
+        self._debug_logger = py2.DebugLogger('WorkerBase')
+
         # Public
         self.worker_id = worker_id
         self.pull_address = pull_from_address
@@ -59,16 +67,58 @@ class WorkerBase(object):
         self._pushed = {}
         self._last_pushed = time.time()
 
+    def _make_listener_thread(self):
+
+        def _listener_thread():
+            while True:
+                # Die if required to
+                if self._stop_listening_event.is_set():
+                    break
+                # Pull only when the pulled from server event is cleared
+                if not self._successfully_pulled_from_server.is_set():
+                    # This blocks until the pull was successful
+                    self.pull()
+                else:
+                    time.sleep(self._listener_latency)
+                    continue
+                # Set event to say the pull was successful
+                self._successfully_pulled_from_server.set()
+
+        return threading.Thread(target=_listener_thread)
+
+    def listen(self):
+        self._stop_listening_event.clear()
+        self._listener_thread = self._make_listener_thread()
+        self._listener_thread.start()
+
+    def stop_listening(self):
+        self._stop_listening_event.set()
+
     def work(self):
         raise NotImplementedError
 
+    def update(self):
+        # Computations go here
+        # ...
+        # When parameter update is done in the subclass, clear event such that listner is allowed
+        # to pull again.
+        self._successfully_pulled_from_server.clear()
+
     def from_config(self, config_file):
         config = ndu.yaml2dict(config_file)
-
         self.worker_id = config.get('worker_id', self.worker_id)
         self.pull_address = config.get('pull_from_address', self.pull_address)
         self.push_address = config.get('push_to_address', self.push_address)
+        return config
 
 
 class Worker(WorkerBase):
+    def __init__(self, **base_kwargs):
+        super(Worker, self).__init__(**base_kwargs)
+        # TODO
+
+    def from_config(self, config_file):
+        config = super(Worker, self).from_config(config_file)
+        # TODO
+
     pass
